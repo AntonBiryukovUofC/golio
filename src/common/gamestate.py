7ec336dfcf39328c161ed303af473903359e3460
Matthew Brigdan <mbrigdan@gmail.com>
2020-04-23 14:21:51 -0600

    Add conversions to/from numpy array and fix apply subboard

--- src/common/gamestate.py
+++ src/common/gamestate.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+import numpy as np
 from .cell import Cell
 
 
@@ -80,8 +81,42 @@ class GameState:
         return new_state
 
     def apply_subboard(self, y, x, subboard):
+        new_state = GameState.copy_gamestate(self)
+
         for ii in range(0, subboard.y):
             for jj in range(0, subboard.x):
-                self[y+ii, x+jj] = subboard[ii,jj]
+                new_state[y+ii, x+jj] = subboard[ii,jj]
 
+        return  new_state
 
+    def get_numpy_array(self):
+        new_array = []
+        for ii in range(0, self.y):
+            new_array.append([-1]*self.x)
+            for jj in range(0, self.x):
+                if self[ii, jj] is not None:
+                    new_array[ii][jj] = self[ii, jj].team_id
+                else:
+                    new_array[ii][jj] = 0
+        np_array = np.array(new_array)
+        return np_array
+
+    @staticmethod
+    def gamestate_from_numpy_array(array):
+        y = array.shape[0]
+        x = array.shape[1]
+        state = GameState(y, x)
+
+        for ii in range(0, y):
+            for jj in range(0, x):
+                if array[ii, jj] != 0:
+                    state[ii, jj] = Cell(team_id=array[ii, jj])
+
+    @staticmethod
+    def copy_gamestate(gamestate: 'GameState'):
+        new_state = GameState(gamestate.y, gamestate.x)
+        for ii in range(0, gamestate.y):
+            for jj in range(0, gamestate.x):
+                new_state[ii, jj] = gamestate[ii,jj]
+
+        return new_state
